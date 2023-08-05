55840c8874cde8c2e36b144bcc818fc658c34598
Zaxiquej <2932734275@qq.com>
2023-08-05 16:16:49 +0800

    test

--- test.js
+++ test.js
@@ -1,40 +1,32 @@
 // 定义API地址
 const apiUrl = "https://shadowverse-portal.com/api/v1/cards";
 
-// 创建一个新的XMLHttpRequest对象
-var xhr = new XMLHttpRequest();
-
-// 配置GET请求
-xhr.open("GET", apiUrl, true);
-
-// 设置API响应的数据类型为JSON
-xhr.responseType = "json";
-
-// 监听请求完成的事件
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            // 成功接收到API响应，处理数据并展示
-            displayCardInfo(xhr.response);
-        } else {
-            // 请求失败，处理错误
-            console.error("请求失败：" + xhr.status);
+// 获取卡牌信息并展示
+fetch(apiUrl)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("请求失败：" + response.status);
         }
-    }
-};
-
-// 发送请求
-xhr.send();
+        return response.json();
+    })
+    .then((data) => {
+        displayCardInfo(data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 
 function displayCardInfo(cardsData) {
     // 在上面的HTML中定义的容器元素
     const container = document.getElementById("card-info-container");
 
     // 循环遍历每张卡牌，并在容器中显示信息
-    for (const card of cardsData.cards) {
+    for (const card of cardsData.data.cards) {
         const cardInfo = document.createElement("div");
-        cardInfo.innerHTML = `<h2>${card.card_name}</h2>
-                              <p>id：${card.card_id}</p>
+        cardInfo.innerHTML = `<h2>${card.name}</h2>
+                              <p>PP消耗：${card.cost}</p>
+                              <p>攻击力：${card.atk}</p>
+                              <p>生命值：${card.life}</p>
                               <hr>`;
 
         container.appendChild(cardInfo);
