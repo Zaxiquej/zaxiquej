5190d387df1f9133061f682b0bf8ed4014ce22c9
Zaxiquej <2932734275@qq.com>
2023-08-05 17:29:14 +0800

    test

--- test.js
+++ test.js
@@ -1,34 +1,30 @@
-// 定义API地址
-const apiUrl = "https://shadowverse-portal.com/api/v1/cards";
+function generateSVG() {
+  const numberInput = document.getElementById('numberInput').value;
+  const svgdbURL = `https://svgdb.me/assets/fullart/${numberInput}.png`;
 
-// 获取卡牌信息并展示
-fetch(apiUrl)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("请求失败：" + response.status);
-        }
-        return response.json();
-    })
-    .then((data) => {
-        displayCardInfo(data);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+  // Create an image element and set the src attribute to the generated SVGDB URL
+  const imageElement = document.createElement('img');
 
-function displayCardInfo(cardsData) {
-    // 在上面的HTML中定义的容器元素
-    const container = document.getElementById("card-info-container");
+  // Add an event listener to check if the image loaded successfully
+  imageElement.addEventListener('load', function() {
+    // Clear the previous image (if any) and add the new image to the container
+    const imageContainer = document.getElementById('imageContainer');
+    imageContainer.innerHTML = '';
+    imageContainer.appendChild(imageElement);
+  });
 
-    // 循环遍历每张卡牌，并在容器中显示信息
-    for (const card of cardsData.data.cards) {
-        const cardInfo = document.createElement("div");
-        cardInfo.innerHTML = `<h2>${card.name}</h2>
-                              <p>PP消耗：${card.cost}</p>
-                              <p>攻击力：${card.atk}</p>
-                              <p>生命值：${card.life}</p>
-                              <hr>`;
+  // Add an event listener to check for errors while loading the image
+  imageElement.addEventListener('error', function() {
+    // Clear the previous image (if any)
+    const imageContainer = document.getElementById('imageContainer');
+    imageContainer.innerHTML = '';
 
-        container.appendChild(cardInfo);
-    }
+    // Display error message
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = '未找到对应图片，请输入正确的数字。';
+    imageContainer.appendChild(errorMessage);
+  });
+
+  // Set the src attribute to the SVGDB URL
+  imageElement.src = svgdbURL;
 }
