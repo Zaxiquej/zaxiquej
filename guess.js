b98724762a692e53cab5b0795e990e7d36ca9d71
Zaxiquej <2932734275@qq.com>
2023-08-06 22:52:08 +0800

    北京时间布丁

--- guess.js
+++ guess.js
@@ -384,6 +384,10 @@ function getNumCommonChars(str1, str2) {
 
 function generateCurrentDateSeed() {
   const now = new Date();
+
+  // 转换为北京时间
+  now.setHours(now.getHours() + 8);
+
   const year = now.getUTCFullYear().toString();
   const month = (now.getUTCMonth() + 1).toString().padStart(2, '0');
   const day = now.getUTCDate().toString().padStart(2, '0');
