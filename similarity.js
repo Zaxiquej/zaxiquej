2228ac838908dff0a523eb48ce6bb8f3b72ea8b8
Zaxiquej <2932734275@qq.com>
2023-08-06 19:07:51 +0800

    小调整

--- similarity.js
+++ similarity.js
@@ -1,18 +1,29 @@
-// 计算基础信息的相似分
 function calculateBasicScore(card1, card2) {
     const weights = {
-        cost: 1,
-        clan: 1,
-        tribe_name: 2,
-        rarity: 1,
-        char_type: 1,
-        atk: 1,
-        life: 1,
-        evo_atk: 1,
-        evo_life: 1,
+        cost: 2,
+        clan: 2,
+        tribe_name: 1,
+        char_type: 4,
     };
 
+    const attributeWeights = {
+        atk: 0.8,
+        life: 0.8,
+        evo_atk: 0.8,
+        evo_life: 0.8,
+    };
+
+    const rarityWeights = {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+    };
+
+    const attributes = ["atk", "life", "evo_atk", "evo_life"];
+
     let basicScore = 0;
+    let attributeScore = 0;
 
     // 对比基础信息，并根据权重计算基础分数
     for (const key in weights) {
@@ -26,9 +37,36 @@ function calculateBasicScore(card1, card2) {
         }
     }
 
-    return basicScore*10;
-  }
+    // 计算稀有度的相似分数
+    if ("rarity" in card1 && "rarity" in card2) {
+        const rarity1 = card1["rarity"];
+        const rarity2 = card2["rarity"];
+        if (rarity1 in rarityWeights && rarity2 in rarityWeights) {
+            const rarityScore = Math.abs(rarityWeights[rarity1] - rarityWeights[rarity2]);
+            basicScore += 1/(rarityScore+1);
+        }
+    }
 
+    // 计算随从属性的相似分数
+    if (card1.char_type == 1 && card2.char_type == 1){ //都是随从，额外考虑身材
+      for (const attribute of attributes) {
+          if (attribute in card1 && attribute in card2) {
+              const value1 = card1[attribute];
+              const value2 = card2[attribute];
+              // 简单的对比方式，可以根据实际需求进行更复杂的对比
+              if (value1 && value2) {
+                  attributeScore -= attributeWeights[attribute]; //补正
+                  attributeScore += attributeWeights[attribute]/(1 + Math.abs(value1 - value2));
+              }
+          }
+      }
+    }
+
+    // 将随从属性的相似分数加入基础分数
+    basicScore += attributeScore;
+
+    return basicScore * 10;
+}
   function calculateDescriptionScore(card1, card2) {
       // Calculate description similarity
       let description1 = card1.skill_disc || '';
