c67c9e521e61773da93deecd8f0c22051436375c
Zaxiquej <2932734275@qq.com>
2023-08-07 09:33:54 +0800

    refractor 可以显示正确答案

--- vaniascript.js
+++ vaniascript.js
@@ -84,81 +84,6 @@ let numButtons = 2;
 
 let currentAudio; // 用于存储当前正在播放的音频对象
 
-// 创建游戏按钮
-function createGameButtons(num, creaturesArray) {
-  const gameButtons = document.getElementById('gameButtons'); // 修改这里的id为gameButtons
-  gameButtons.innerHTML = ''; // 清空原有的按钮
-
-  // 随机选择num种创造物图片按钮
-  randomCreatures = shuffleArray(creaturesArray).slice(0, num);
-
-  randomCreatures.forEach((creature, index) => {
-    const img = document.createElement('img');
-    img.src = creature.image;
-    img.alt = `创造物${creature.id}`;
-    img.addEventListener('click', () => checkAnswer(creature));
-    gameButtons.appendChild(img);
-  });
-}
-
-
-// 打乱数组的顺序
-function shuffleArray(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
-// 播放音乐
-function playMusic(sound) {
-  if (currentAudio) {
-    currentAudio.pause(); // 暂停当前正在播放的音频
-    currentAudio.currentTime = 0; // 将音频播放位置重置为0
-  }
-
-  const audio = new Audio(sound);
-  audio.play().catch(error => {
-    // 防止浏览器的自动播放策略导致无法播放声音
-    console.error('播放音乐失败：', error);
-  });
-
-  currentAudio = audio; // 将当前播放的音频对象保存到全局变量
-  isPlaying = true;
-
-  audio.onended = () => {
-    isPlaying = false;
-    if (isSoundRepeating) {
-      playMusic(sound); // 重复播放音乐
-    }
-  };
-}
-
-// 检查答案是否正确
-function checkAnswer(selectedCreature) {
-  //if (isPlaying) return;
-
-  if (timeLimit > 0) {
-    const currentCreature = randomCreatures[randNum];
-    if (selectedCreature.id === currentCreature.id) {
-      score += 1;
-      document.getElementById('score').innerText = `分数: ${score}`;
-      increaseDifficulty();
-    } else {
-      endGame();
-    }
-  }
-}
-
-// 获取当前播放的创造物ID
-function getCurrentCreatureId() {
-  return randomCreatures[randNum].id;
-}
-
-function getRandomInt(n) {
-  // 使用 Math.random() 生成一个 [0, 1) 之间的随机浮点数
-  // 将其乘以 n，得到 [0, n) 之间的随机浮点数
-  // 使用 Math.floor() 向下取整，得到 [0, n-1] 之间的随机整数
-  return Math.floor(Math.random() * n);
-}
-
 // 增加难度
 function increaseDifficulty() {
   if (score <= 11){
@@ -187,27 +112,6 @@ function increaseDifficulty() {
   currentcreatures = randomCreatures[getCurrentCreatureId()];
   playMusic(randomCreatures[randNum].sound)
 }
-function replaySound(){
-  playMusic(randomCreatures[randNum].sound)
-}
-
-// 结束游戏
-function endGame() {
-  clearInterval(gameInterval);
-  timeLimit = 0;
-  const gameButtons = document.getElementById('gameButtons'); // 修改这里的id为gameButtons
-  gameButtons.innerHTML = ''; // 清空原有的按钮
-  const Buttons = document.getElementById('Buttons');
-  Buttons.innerHTML = `<p>游戏结束，你的得分是：${score}</p><button class="center-button" onclick="restartGame()">重新开始</button>`;
-  document.getElementById('startButton').style.display = 'none'; // 不显示"开始游戏"按钮
-  document.getElementById('replayButton').style.display = 'none'; // 隐藏"开始游戏"按钮
-  document.getElementById('other').style.display = 'block'; // 隐藏"其他"按钮
-  // 暂停并重置当前正在播放的音频
-  if (currentAudio) {
-    currentAudio.pause();
-    currentAudio.currentTime = 0;
-  }
-}
 
 // 重新开始游戏
 function restartGame() {
@@ -232,118 +136,9 @@ function restartGame() {
   playMusic(randomCreatures[randNum].sound)
 }
 
-// 更新倒计时
-function updateTimeLimit() {
-  if (timeLimit > 0) {
-    timeLimit -= 1;
-    document.getElementById('timer').innerText = `剩余时间: ${timeLimit}`;
-    if (timeLimit <= 0) {
-      endGame();
-    }
-  }
-}
-
-// 更新倒计时
-function refreshTimeLimit() {
-    document.getElementById('timer').innerText = `剩余时间: ${timeLimit}`;
-}
-
-// 声音重复播放按钮功能
-function toggleSound() {
-  isSoundRepeating = !isSoundRepeating;
-}
-
 // 全局变量，用于跟踪已加载的素材数量
 let loadedCount = 0;
-let total
-
-// 预加载图片
-function preloadImages(images) {
-  return Promise.all(images.map((image) => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = image;
-      img.onload = () => {
-        loadedCount++;
-        resolve();
-        updateLoadingProgress(total,loadedCount);
-      };
-      img.onerror = () => reject();
-    });
-  }));
-}
-
-// 预加载声音
-function preloadSounds(sounds) {
-  return Promise.all(sounds.map((sound) => {
-    return new Promise((resolve, reject) => {
-      const audio = new Audio();
-      audio.src = sound;
-      audio.preload = 'auto';
-      audio.onloadeddata = () => {
-        loadedCount++;
-        resolve();
-        updateLoadingProgress(total,loadedCount);
-      };
-      audio.onerror = () => reject();
-    });
-  }));
-}
-
-function updateLoadingProgress(total, loaded) {
-  const loadingBar = document.getElementById('loadingBar');
-  const progress = (loaded / total) * 100;
-  loadingBar.style.width = `${progress}%`;
-
-  if (loaded === total) {
-    // 当所有素材加载完成后，隐藏进度条和显示"开始游戏"按钮
-    const loadingContainer = document.getElementById('loadingContainer');
-    const startButton = document.getElementById('startButton');
-
-    loadingContainer.style.display = 'none';
-    if (document.getElementById('other').style.display != 'none'){
-      startButton.style.display = 'block';
-    }
-  }
-}
-
-// 预加载所有图片和声音
-function preloadAll() {
-  const allCreatureImages = creatures.map((creature) => creature.image);
-  const allEvolvedCreatureImages = evolvedCreatures.map((creature) => creature.image);
-  const allBasicCreatureImages = basic.map((creature) => creature.image);
-  const allImages = allCreatureImages.concat(allEvolvedCreatureImages).concat(allBasicCreatureImages);
-
-  const allCreatureSounds = creatures.map((creature) => creature.sound);
-  const allEvolvedCreatureSounds = evolvedCreatures.map((creature) => creature.sound);
-  const allBasicCreatureSounds = basic.map((creature) => creature.sound);
-  const allSounds = allCreatureSounds.concat(allEvolvedCreatureSounds).concat(allBasicCreatureSounds);
-
-  total = allImages.length + allSounds.length;
-
-    Promise.all([preloadImages(allImages), preloadSounds(allSounds)]).then(() => {
-    // 当所有素材加载完成后，更新进度条状态
-    updateLoadingProgress(allImages.length + allSounds.length, allImages.length + allSounds.length);
-  }).catch(() => {
-    console.error('预加载失败！');
-  });
-}
-
-// 初始化游戏
-function initGame() {//
-      // 显示加载状态信息和进度条
-  const loadingContainer = document.getElementById('loadingContainer');
-  loadingContainer.style.display = 'block';
-  preloadAll();
-}
-
-// 开始游戏
-function startGame() {
-    restartGame();
-    //gameInterval = setInterval(updateTimeLimit, 1000);
-    document.getElementById('startButton').style.display = 'none'; // 隐藏"开始游戏"按钮
-    document.getElementById('replayButton').style.display = 'block'; // 隐藏"开始游戏"按钮
-}
+let total;
 
 // 初始化游戏
 createGameButtons(0, []); // 初始时没有按钮
