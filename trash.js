10ee406f1d9304bc6f5a67c5bbb18ac84b012499
Zaxiquej <2932734275@qq.com>
2023-08-21 18:27:56 -0400

    123

--- trash.js
+++ trash.js
@@ -1,110 +1,142 @@
-let resources = {
-  metal: 0,
-  wood: 0,
-  water: 0,
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const keyboard = document.querySelector(".keyboard");
 
-let buildings = {
-  building1: {
-    count: 0,
-    cost: 10,
-    consumption: {
-      metal: 1,
-    },
-    production: {
-      metal: 1,
-    },
-  },
+    const createKeyButton = (keyName, imageName, soundIndex) => {
+        const keyButton = document.createElement("button");
+        keyButton.dataset.soundIndex = soundIndex; // Store sound index
+        keyButton.style.backgroundImage = `url(image/${imageName})`;
 
-  building2: {
-    count: 0,
-    cost: 15,
-    consumption: {
-      wood: 1,
-    },
-    production: {
-      wood: 1,
-    },
-  },
+        const keyLabel = document.createElement("span");
+        keyLabel.textContent = keyName;
 
-  building3: {
-    count: 0,
-    cost: 20,
-    consumption: {
-      metal: 1,
-    },
-    production: {
-      water: 1,
-    },
-  },
-};
+        keyButton.appendChild(keyLabel);
 
-function updateResourcesDisplay() {
-  document.getElementById('metal').textContent = resources.metal;
-  document.getElementById('wood').textContent = resources.wood;
-  document.getElementById('water').textContent = resources.water;
-}
+        keyButton.addEventListener("click", () => {
+            const newAudioElement = new Audio(`sound/sound${soundIndex}.mp3`);
+            newAudioElement.volume = volumeSlider.value;
+            newAudioElement.play();
 
-function updateBuildingDisplay(buildingName) {
-  const building = buildings[buildingName];
-  document.getElementById(`${buildingName}Count`).textContent = building.count;
-  document.getElementById(`${buildingName}Cost`).textContent = building.cost;
-  document.getElementById(`${buildingName}Consumption`).textContent = Object.keys(building.consumption).join(', ');
-}
+            // Store active audio element
+            activeAudioElements.push(newAudioElement);
+        });
 
-function upgradeBuilding(buildingIndex) {
-  const buildingName = `building${buildingIndex}`;
-  const building = buildings[buildingName];
+        return keyButton;
+    };
 
-  let canUpgrade = true;
-  for (let consumedResource in building.consumption) {
-    if (resources[consumedResource] < building.consumption[consumedResource]) {
-      canUpgrade = false;
-      break;
+    const keyboardContainer = document.querySelector(".keyboard"); // Add this line
+
+    const audioPlayer = document.querySelector("#audioPlayer");
+    const activeAudioElements = [];
+    const keyMappings = [
+        { keyName: "1", imageName: "creature1.png", soundIndex: 1 },
+        { keyName: "2", imageName: "creature2.png", soundIndex: 2 },
+        { keyName: "3", imageName: "creature3.png", soundIndex: 3 },
+        { keyName: "4", imageName: "creature4.png", soundIndex: 4 },
+        { keyName: "5", imageName: "creature5.png", soundIndex: 5 },
+        { keyName: "6", imageName: "creature6.png", soundIndex: 6 },
+        { keyName: "7", imageName: "creature7.png", soundIndex: 7 },
+        { keyName: "8", imageName: "creature8.png", soundIndex: 8 },
+        { keyName: "9", imageName: "creature9.png", soundIndex: 9 },
+        { keyName: "0", imageName: "creature10.png", soundIndex: 10 },
+        { keyName: "Q", imageName: "creature101.png", soundIndex: 101 },
+        { keyName: "W", imageName: "creature102.png", soundIndex: 102 },
+        { keyName: "E", imageName: "creature103.png", soundIndex: 103 },
+        { keyName: "R", imageName: "creature104.png", soundIndex: 104 },
+        { keyName: "T", imageName: "creature105.png", soundIndex: 105 },
+        { keyName: "Y", imageName: "creature106.png", soundIndex: 106 },
+        { keyName: "U", imageName: "creature107.png", soundIndex: 107 },
+        { keyName: "I", imageName: "creature108.png", soundIndex: 108 },
+        { keyName: "O", imageName: "creature109.png", soundIndex: 109 },
+        { keyName: "P", imageName: "creature110.png", soundIndex: 110 },
+        { keyName: "A", imageName: "creature11.png", soundIndex: 11 },
+        { keyName: "S", imageName: "creature12.png", soundIndex: 12 },
+        { keyName: "D", imageName: "creature13.png", soundIndex: 13 },
+        { keyName: "F", imageName: "creature14.png", soundIndex: 14 },
+        { keyName: "G", imageName: "creature15.png", soundIndex: 15 },
+        { keyName: "H", imageName: "creature16.png", soundIndex: 16 },
+        { keyName: "J", imageName: "creature17.png", soundIndex: 17 },
+        { keyName: "K", imageName: "creature18.png", soundIndex: 18 },
+        { keyName: "L", imageName: "creature19.png", soundIndex: 19 },
+        { keyName: "Z", imageName: "creature111.png", soundIndex: 111 },
+        { keyName: "X", imageName: "creature112.png", soundIndex: 112 },
+        { keyName: "C", imageName: "creature113.png", soundIndex: 113 },
+        { keyName: "V", imageName: "creature114.png", soundIndex: 114 },
+        { keyName: "B", imageName: "creature115.png", soundIndex: 115 },
+        { keyName: "N", imageName: "creature116.png", soundIndex: 116 },
+        { keyName: "M", imageName: "creature117.png", soundIndex: 117 },
+        { keyName: ",", imageName: "creature118.png", soundIndex: 118 },
+        { keyName: ".", imageName: "creature119.png", soundIndex: 119 }
+    ];
+
+    keyMappings.forEach((mapping) => {
+        const button = document.createElement("button");
+        button.textContent = mapping.keyName;
+
+        // Set background image
+        const backgroundImageUrl = `url(image/creature${mapping.soundIndex}.png)`;
+        button.style.backgroundImage = backgroundImageUrl;
+        button.style.backgroundBlendMode = "multiply"; // Combine image and gradient with multiply blend mode
+
+
+        button.addEventListener("click", () => playSound(mapping.soundIndex));
+        keyboardContainer.appendChild(button);
+    });
+
+    const globalVolumeSlider = document.querySelector("#globalVolumeSlider");
+    const globalFrequencySlider = document.querySelector("#globalFrequencySlider");
+    const globalVolumeValue = document.querySelector("#globalVolumeValue");
+    const globalFrequencyValue = document.querySelector("#globalFrequencyValue");
+
+    globalVolumeSlider.addEventListener("input", (event) => {
+        const globalVolume = event.target.value;
+        audioPlayer.volume = globalVolume;
+        globalVolumeValue.textContent = globalVolume;
+    });
+
+    globalFrequencySlider.addEventListener("input", (event) => {
+        const globalFrequency = event.target.value;
+        activeAudioElements.forEach((element) => {
+            element.playbackRate = globalFrequency / 100;
+        });
+        globalFrequencyValue.textContent = globalFrequency;
+    });
+
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    function playSound(soundIndex) {
+        // Create a new audio buffer source
+        const audioSource = audioContext.createBufferSource();
+
+        // Load and decode the audio file
+        fetch(`sound/sound${soundIndex}.mp3`)
+            .then(response => response.arrayBuffer())
+            .then(data => audioContext.decodeAudioData(data))
+            .then(decodedBuffer => {
+                audioSource.buffer = decodedBuffer;
+                audioSource.connect(audioContext.destination);
+                audioSource.volume = audioPlayer.volume; // Set the volume
+                audioSource.playbackRate = globalFrequencySlider.value / 100; // Set the playback rate
+                audioSource.start(0);
+            })
+            .catch(error => console.error(error));
     }
-  }
 
-  if (canUpgrade) {
-    for (let consumedResource in building.consumption) {
-      resources[consumedResource] -= building.consumption[consumedResource];
-    }
+    // Enable keyboard key press
+    window.addEventListener("keydown", (event) => {
+        const keyMapping = keyMappings.find(
+            (mapping) => mapping.keyName === event.key.toUpperCase()
+        );
+        if (keyMapping) {
+            const { soundIndex } = keyMapping;
+            playSound(soundIndex); // Call the playSound function with the soundIndex
+        }
+    });
 
-    building.count++;
-    building.cost = Math.round(building.cost * (1 + 0.1 * building.count));
-    updateBuildingDisplay(buildingName);
-    updateResourcesDisplay();
-  }
-}
-
-function manualResourceGain(resourceType) {
-  if (resourceType !== 'water') {
-    resources[resourceType] += 1;
-    updateResourcesDisplay();
-  }
-}
-
-function updateProduction() {
-  for (let buildingName in buildings) {
-    const building = buildings[buildingName];
-
-    let canOperate = true;
-    for (let consumedResource in building.consumption) {
-      if (resources[consumedResource] < building.consumption[consumedResource] * building.count) {
-        canOperate = false;
-        break;
-      }
-    }
-
-    if (canOperate) {
-      for (let producedResource in building.production) {
-        resources[producedResource] += building.production[producedResource] * building.count;
-      }
-    }
-
-    updateBuildingDisplay(buildingName);
-  }
-
-  updateResourcesDisplay();
-}
-
-setInterval(updateProduction, 1000);
+    // Pause previously active audio elements when ended
+    audioPlayer.addEventListener("ended", () => {
+        activeAudioElements.forEach((element) => {
+            element.pause();
+        });
+        activeAudioElements.length = 0;
+    });
+});
