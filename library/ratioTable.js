dde3f830ea831aa4b0db4d71eca9b8695218f863
Zaxiquej <2932734275@qq.com>
2023-08-24 21:42:01 -0400

    999

--- library/ratioTable.js
+++ library/ratioTable.js
@@ -164,7 +164,7 @@ let ratioTable = {
   "drain": {reward:1.5, punish:1.5},
   "evolution_end_stop": {reward:2, punish:2}, //进化前后关键词不同
   "fusion": {reward:4, punish:2}, //融合
-  "type=oldest": {reward:1.5, punish:0.5}, //轮流伤害
+  "type=oldest": {reward:1.5, punish:0.4}, //轮流伤害
   "attack_count": {reward:2.2, punish:2.2}, //连击
   "change_rally_count": {reward:6, punish:2}, //改变连协
   "not_be_attacked": {reward:2.5, punish:1.5}, //物免
