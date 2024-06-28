daa3aa14d2c5b0aa05fb9ab97503ed60c3dce8c7
Zaxiquej <2932734275@qq.com>
2024-06-28 13:53:58 +0800

    12345

--- kmr/game.js
+++ kmr/game.js
@@ -2579,7 +2579,7 @@ function raiseAtk(minion, amount, norepeat, fromUpgrade) {
      }
    }
    if (fromUpgrade && minion.description.includes("🐷") && m.learnedSkills.includes("老实猪猪")) {
-       amount = amount.times(1.2);
+       amount = amount.times(1 + 0.05 * getDigit(minion.attack));
    }
  }
  if (fromUpgrade) {
