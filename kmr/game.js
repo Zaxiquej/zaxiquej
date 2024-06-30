f459ee93920c5503a5240d82eb8d5c275922117a
Zaxiquej <2932734275@qq.com>
2024-06-30 18:18:32 +0800

    123

--- kmr/game.js
+++ kmr/game.js
@@ -13,7 +13,7 @@ const totalTimeDisplay2 = document.getElementById('total-time2');
 const curLevelDisplay = document.getElementById('total-level');
 const finalStatsDisplay = document.getElementById('final-stats');
 
-let version = "3.0.7";
+let version = "3.1.0";
 let kmrHealthValue = new Decimal('500000');
 let level = 0;
 let coins = new Decimal('0');
@@ -350,7 +350,7 @@ function hardResetVars() {
 }
 
 function resetVars() {
-  version = "3.0.7";
+  version = "3.1.0";
   kmrHealthValue = new Decimal('500000');
   level = 0;
   coins = new Decimal('0');
